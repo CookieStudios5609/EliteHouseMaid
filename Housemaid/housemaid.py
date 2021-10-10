7bb8e9bfdc32a64d7860ffd8c0183c7638bff1b6
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-10-10 07:35:24 -0400

    Feat: Add more verbose exception handling to extension loader

--- Housemaid/housemaid.py
+++ Housemaid/housemaid.py
@@ -7,11 +7,11 @@ import os
 
 dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+TEST_GUILD = os.getenv("TEST_GUILD")
 
 intents = disnake.Intents.default()
 prefix = ">>"
-bot = commands.Bot(command_prefix=prefix, intents=intents, test_guilds=[])
+bot = commands.Bot(command_prefix=prefix, intents=intents, test_guilds=[TEST_GUILD])
 logging.basicConfig(level='INFO', filename='maidlog.txt', filemode='w')
 startup_cogs = ['cogs.owner']
 
@@ -21,8 +21,10 @@ if __name__ == "__main__":
     for extension in startup_cogs:
         try:
             bot.load_extension(extension)
-        except: # fix bare except
-            logging.error(f"Loading {extension} failed.")
+        except ModuleNotFoundError as e:
+            logging.error(f"Extension {extension} was not found.")
+        except Exception as e:  # fix bare except
+            logging.error(f"Error occurred while loading {extension}: {e}")
         else:
             logging.info(f"Started {extension}...")
 

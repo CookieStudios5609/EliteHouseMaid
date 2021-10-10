925acec5c3d62075c2568957735f59d4f8af6aa7
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-10-10 07:34:39 -0400

    Feat: Progress on bookmark message interaction

--- Housemaid/cogs/owner.py
+++ Housemaid/cogs/owner.py
@@ -10,11 +10,13 @@ class Owner(commands.Cog):
         return await ctx.bot.is_owner(ctx.author)
 
     @commands.message_command(name="Bookmark This!")
-    async def bookmark_message(self, inter: disnake.MessageCommandInteraction):
+    async def bookmark_message(self, inter: disnake.MessageCommandInteraction) -> None:
         try:
-            await inter.author.send(f"Work in Progress\n{inter.data}")
+            await inter.author.send(f"Work in Progress\n{inter.author} in {inter.channel_id} in {inter.guild_id} e {inter.target.content} {inter.target.channel}")
+            await inter.response.send_message("Bookmarked successfully!", ephemeral=True)
         except disnake.errors.Forbidden:
-            await inter.response.send_message("LOL BOOKMARK FAILURE BECAUSE DMS ARE OFF! Laugh at this user!")
+
+            await inter.response.send_message(f"LOL {inter.target.author.mention}, SOME CLOWN NAMED {inter.author.mention} TRIED TO BOOKMARK YOUR MESSAGE, BUT HAS DMS OFF! Laugh at this user!")
 
 
 def setup(bot):
